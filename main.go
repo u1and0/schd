9d866f1fc8240a8e65eca58a94f8abddd00269dc
u1and0 <e01.ando60@gmail.com>
2022-03-30 21:06:55 +0900

    [add] DELETE method

--- main.go
+++ main.go
@@ -33,14 +33,13 @@ func main() {
 				c.IndentedJSON(http.StatusOK, data)
 			})
 			d.GET("/:id", func(c *gin.Context) {
-				s := c.Param("id")
-				id := ksn.ID(s)
+				id := ksn.ID(c.Param("id"))
 				if datum, ok := data[id]; ok { // Cast
 					c.IndentedJSON(http.StatusOK, datum)
 					return
 				}
 				c.JSON(http.StatusBadRequest,
-					gin.H{"msg": fmt.Sprintf("%v not found", id)})
+					gin.H{"error": fmt.Sprintf("%v not found", id)})
 			})
 			d.GET("/cal", func(c *gin.Context) {
 				cal := data.Stack()
@@ -53,13 +52,13 @@ func main() {
 			d.POST("/post", func(c *gin.Context) {
 				var addData ksn.Data
 				if err := c.ShouldBindJSON(&addData); err != nil {
-					c.JSON(http.StatusBadRequest, gin.H{"msg": err.Error()})
+					c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 				}
 				// Data exist check
 				for k := range addData {
 					if _, ok := data[k]; ok {
 						msg := fmt.Sprintf("ID: %v データが既に存在しています。Updateを試してください。", k)
-						c.JSON(http.StatusBadRequest, gin.H{"msg": msg})
+						c.JSON(http.StatusBadRequest, gin.H{"error": msg})
 						return
 					}
 				}
@@ -68,6 +67,24 @@ func main() {
 					data[k] = v
 				}
 				c.IndentedJSON(http.StatusOK, addData)
+			})
+			d.DELETE("/:id", func(c *gin.Context) {
+				id := ksn.ID(c.Param("id"))
+				if _, ok := data[id]; !ok {
+					msg := fmt.Sprintf("ID: %v が見つかりません。別のIDを指定してください。", id)
+					c.JSON(http.StatusBadRequest, gin.H{"error": msg})
+					return
+				}
+				delete(data, id)
+				// Check deleted id
+				if _, ok := data[id]; ok {
+					msg := fmt.Sprintf("ID: %v を削除できませんでした。", id)
+					c.JSON(http.StatusBadRequest, gin.H{"error": msg})
+					return
+				}
+				msg := fmt.Sprintf("ID: %v を削除しました。", id)
+				c.JSON(http.StatusOK, gin.H{"msg": msg})
+				return
 			})
 		}
 
