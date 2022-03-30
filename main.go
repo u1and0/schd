9234471e9a0b5aff88cd32c1cc33dc4d3ac04f80
u1and0 <e01.ando60@gmail.com>
2022-03-30 16:06:26 +0900

    [add] post method

--- main.go
+++ main.go
@@ -39,7 +39,8 @@ func main() {
 					c.IndentedJSON(http.StatusOK, datum)
 					return
 				}
-				c.IndentedJSON(http.StatusBadRequest, gin.H{"msg": fmt.Sprintf("%v not found", id)})
+				c.JSON(http.StatusBadRequest,
+					gin.H{"msg": fmt.Sprintf("%v not found", id)})
 			})
 			d.GET("/cal", func(c *gin.Context) {
 				cal := data.Stack()
@@ -48,6 +49,25 @@ func main() {
 			d.GET("/list", func(c *gin.Context) {
 				rows := data.Stack().Unstack()
 				c.IndentedJSON(http.StatusOK, rows)
+			})
+			d.POST("/post", func(c *gin.Context) {
+				var addData ksn.Data
+				if err := c.ShouldBindJSON(&addData); err != nil {
+					c.JSON(http.StatusBadRequest, gin.H{"msg": err.Error()})
+				}
+				// Data exist check
+				for k := range addData {
+					if _, ok := data[k]; ok {
+						msg := fmt.Sprintf("ID: %v データが既に存在しています。Updateを試してください。", k)
+						c.JSON(http.StatusBadRequest, gin.H{"msg": msg})
+						return
+					}
+				}
+				// Append data
+				for k, v := range addData {
+					data[k] = v
+				}
+				c.IndentedJSON(http.StatusOK, addData)
 			})
 		}
 
