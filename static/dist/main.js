10e792bd6cca4f96777d1d6109188a0261515f32
u1and0 <e01.ando60@gmail.com>
2022-05-19 07:18:02 +0900

    [add] feature textarea

--- static/dist/main.js
+++ static/dist/main.js
@@ -1,4 +1,5 @@
 import { Fzf } from "../node_modules/fzf/dist/fzf.es.js";
+import { InputSuggest } from "../node_modules/input-suggest/dist/input-suggest.min.js";
 const root = new URL(window.location.href);
 export const url = root.origin + "/api/v1/data";
 export let searchers;
@@ -7,6 +8,9 @@ main();
 async function main() {
     searchers = await fetchPath(url + "/allocate/list");
     allocations = await fetchPath(url + "/allocates");
+    const textarea = new InputSuggest("textarea");
+    InputSuggest("textarea");
+    textarea.setSuggestions(["foo", "bar", "biz"]);
 }
 // fetchの返り値のPromiseを返す
 async function fetchPath(url) {
--- static/src/main.ts
+++ static/src/main.ts
@@ -1,4 +1,5 @@
 import { Fzf } from "../node_modules/fzf/dist/fzf.es.js";
+import { InputSuggest } from "../node_modules/input-suggest/dist/input-suggest.min.js";
 
 const root: URL = new URL(window.location.href);
 export const url: string = root.origin + "/api/v1/data";
@@ -16,6 +17,9 @@ type Searcher = {
 async function main() {
   searchers = await fetchPath(url + "/allocate/list");
   allocations = await fetchPath(url + "/allocates");
+  const textarea = new InputSuggest("textarea");
+  InputSuggest("textarea");
+  textarea.setSuggestions(["foo", "bar", "biz"]);
 }
 
 // fetchの返り値のPromiseを返す
