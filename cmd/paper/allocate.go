bcddf0e867813ae41ce4e1c56db7ebe1656db610
u1and0 <e01.ando60@gmail.com>
2022-05-22 11:24:05 +0900

    [mod] Cells Set value

--- cmd/paper/allocate.go
+++ cmd/paper/allocate.go
@@ -42,130 +42,110 @@ func CreateAllocate(c *gin.Context) {
 	o := new(api.Allocation)
 	if err := c.Bind(&o); err != nil {
 		msg := err.Error()
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"msg": msg, "error": err})
+		c.JSON(http.StatusBadRequest, gin.H{"msg": msg, "error": err})
 		return
 	}
 	fmt.Printf("Allocation: %#v\n", o)
 
 	// template XLSX
 	f, err := excelize.OpenFile("template/template.xlsx")
+	defer f.Close()
 	if err != nil {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"msg": err.Error(), "error": err})
+		c.JSON(http.StatusBadRequest, gin.H{"msg": err.Error(), "error": err})
 		return
 	}
 	sheetName := "入力画面"
 	// 要求番号
 	reqNo, err := getRequestNo(o.Section)
 	if err != nil {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"msg": err.Error(), "error": err})
+		c.JSON(http.StatusBadRequest, gin.H{"msg": err.Error(), "error": err})
 		return
 	}
 	cells := Cells{
 		"F2": reqNo.Base,
-		"F3": time.Now().Format(LAYOUT), // // 要求年月日
+		"F3": time.Now().Format(LAYOUT), // 要求年月日
 		"F4": o.Section,
-	}
-	if err := cells.SetCellValue(f, sheetName); err != nil {
-		fmt.Println(err)
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"msg": err.Error(), "error": err})
-	}
-	// 輸送便の別
-	s := o.Transport
-	switch s {
-	case "仕立便":
-		s = `☑仕立便　☐常用便
+		"F5": func() string { // 輸送便の別
+			s := o.Transport
+			switch s {
+			case "仕立便":
+				s = `☑仕立便　☐常用便
 ☐混載便　☐宅配便`
-	case "常用便":
-		s = `☐仕立便　☑常用便
+			case "常用便":
+				s = `☐仕立便　☑常用便
 ☐混載便　☐宅配便`
-	case "混載便":
-		s = `☐仕立便　☐常用便
+			case "混載便":
+				s = `☐仕立便　☐常用便
 ☑混載便　☐宅配便`
-	case "宅配便":
-		s = `☐仕立便　☐常用便
+			case "宅配便":
+				s = `☐仕立便　☐常用便
 ☐混載便　☑宅配便`
-	}
-	f.SetCellValue(sheetName, "F5", s)
-	// t数 台車 機能 クラス・ボディータイプ
-	f.SetCellValue(sheetName, "F6", o.Car)
-	// 生産命令番号
-	f.SetCellValue(sheetName, "F7", o.Order)
-	// 輸送区間
-	from, _ := f.GetCellValue(sheetName, "F8")
-	f.SetCellValue(sheetName, "F8", from+o.To.Name)
-	// 送り先
-	// a := strings.Join(ctrl.Config.AddressMap[to], "\n")
-	f.SetCellValue(sheetName, "F13", o.To.Address)
-	// 積込/到着作業月日/時刻
-	x := map[string]interface{}{
+			}
+			return s
+		}(),
+		"F6": o.Car,   // t数 台車 機能 クラス・ボディータイプ
+		"F7": o.Order, // 生産命令番号
+		"F8": func() string { // 輸送区間
+			from, _ := f.GetCellValue(sheetName, "F8")
+			return from + o.To.Name
+		}(),
+		// 積込/到着作業月日/時刻
 		"F9":  o.Load.Date.Format(LAYOUT),
 		"F10": fmt.Sprintf("%d時%d分", o.Load.Hour, o.Load.Minute),
 		"F11": o.Arrive.Date.Format(LAYOUT),
 		"F12": fmt.Sprintf("%d時%d分", o.Arrive.Hour, o.Arrive.Minute),
-	}
-	for cell, value := range x {
-		if err = f.SetCellValue(sheetName, cell, value); err != nil {
-			fmt.Println(err)
-		}
-	}
-	// 保険要否
-	x = map[string]interface{}{
+		// 送り先
+		"F13": o.To.Address,
+		// 保険要否
 		"F18": `☐要　☑不要`, // 保険
 		"F19": "",       // 保険額
 	}
 	if o.Insulance.Need != "契約済み" {
-		x = map[string]interface{}{
-			"F18": `☐要　☑不要`,          // 保険
-			"F19": o.Insulance.Price, // 保険額
-		}
+		cells["F18"] = `☐要　☑不要`          // 保険
+		cells["F19"] = o.Insulance.Price // 保険額
 	}
-	for cell, value := range x {
-		if err = f.SetCellValue(sheetName, cell, value); err != nil {
-			fmt.Println(err)
-		}
+	if err := cells.SetCellValue(f, sheetName); err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusBadRequest, gin.H{"msg": err.Error(), "error": err})
 	}
 	// 寸法質量
 	l := len(o.Style)
 	if l < MAXLINE { // 3行までなら配車要求票に記載
 		p := o.Package.Compile()
 		fmt.Println(p)
-		x := map[string]interface{}{
+		x := Cells{
 			"F15": o.Package.ToString(), // 重量・長さなど
 			"F16": p.ToString(),         // 荷姿(個数)
 			"F17": o.Package.Sum(),      // 総個数
 		}
-		for cell, value := range x {
-			if err = f.SetCellValue(sheetName, cell, value); err != nil {
-				fmt.Println(err)
-			}
+		if err := x.SetCellValue(f, sheetName); err != nil {
+			fmt.Println(err)
+			c.JSON(http.StatusBadRequest, gin.H{"msg": err.Error(), "error": err})
 		}
 	} else { // 4行以上の場合は別紙に記載
-		x := map[string]interface{}{
+		x := Cells{
 			"F15": "別紙参照",
 			"F16": "別紙参照",
 			"F17": o.Package.Sum(),
 		}
-		for cell, value := range x {
-			// 重量・長さなど 荷姿(個数) 総個数
-			if err = f.SetCellValue(sheetName, cell, value); err != nil {
-				fmt.Println(err)
-			}
+		// 重量・長さなど 荷姿(個数) 総個数
+		if err := x.SetCellValue(f, sheetName); err != nil {
+			fmt.Println(err)
+			c.JSON(http.StatusBadRequest, gin.H{"msg": err.Error(), "error": err})
 		}
 		sheetName := "配車別紙"
-		c := 11
 		for i := 0; i < l; i++ {
-			n := strconv.Itoa(c + i)
-			x := map[string]interface{}{
+			n := strconv.Itoa(11 + i) // Line number 11
+			x := Cells{
 				"F" + n: o.Style[i],
 				"G" + n: fmt.Sprintf("%dx%dx%d", o.Width[i], o.Length[i], o.Hight[i]),
 				"J" + n: o.Mass[i],
 				"K" + n: o.Method[i],
 				"L" + n: o.Quantity[i],
 			}
-			for cell, value := range x {
-				if err = f.SetCellValue(sheetName, cell, value); err != nil {
-					fmt.Println(err)
-				}
+			if err := x.SetCellValue(f, sheetName); err != nil {
+				fmt.Println(err)
+				c.JSON(http.StatusBadRequest, gin.H{"msg": err.Error(), "error": err})
 			}
 		}
 	}
@@ -179,7 +159,7 @@ func CreateAllocate(c *gin.Context) {
 		"F11": "" /*物品名称*/, "F12": "" /*重量長さなど*/, "F13": "", /*荷姿*/
 		"F15": "" /*保険*/, "K15": "" /*保険額*/, "O14": "" /*総個数*/, "Q8": "", /*記事*/
 	}
-	err = cells.SetCellValue(f, "付表１　配車要求票")
+	cells.SetCellValue(f, "付表１　配車要求票")
 	cells = Cells{
 		"T1": "" /*要求番号*/, "T2": "" /*要求年月日*/, "Q3": "", /*要求元*/
 		"F3": "" /*輸送便の別*/, "G4": "" /*クラス・ボディタイプ*/, "F5": "", /*生産命令番号*/
@@ -188,7 +168,7 @@ func CreateAllocate(c *gin.Context) {
 		"L8": "" /*到着指定時*/, "F9": "", /*送り先*/
 		"F10": "" /*重量長さなど*/, "F11": "" /*荷姿*/, "O12": "" /*総個数*/, "Q7": "", /*記事*/
 	}
-	err = cells.SetCellValue(f, "付表２　輸送指示書")
+	cells.SetCellValue(f, "付表２　輸送指示書")
 	f.SaveAs(fmt.Sprintf("%s/%s_%s.xlsx", ctrl.Config.AllocatePath, reqNo.Base, o.Package.Name))
 	downloadFile(sheetName+".xlsx", f, c)
 }
