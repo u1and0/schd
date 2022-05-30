abaf167542ec958378b822e7954e49629e385c0f
u1and0 <e01.ando60@gmail.com>
2022-05-30 17:43:30 +0900

    [mod] add/remove table row limit

--- static/dist/row.js
+++ static/dist/row.js
@@ -1,13 +1,17 @@
 function _appendRow(x){
+  if ($(".table-hover tr").length > 20) {
+    alert("これ以上行を追加できません。")
+    return
+  }
   tr = $(x).closest("tr")
   newtr = tr.clone()
   tr.after(newtr)
 }
 
 function _removeRow(x) {
-  if ($("#load-table tr").length > 2) {
-    $(x).closest("tr").remove()
-  } else {
+  if ($(".table-hover tr").length < 3) {
     alert("これ以上行を削除できません。")
+    return
   }
+  $(x).closest("tr").remove()
 }
