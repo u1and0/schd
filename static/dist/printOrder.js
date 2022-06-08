013be87c7c0293cadc2a5e95d94b988a025bd24b
u1and0 <e01.ando60@gmail.com>
2022-06-08 13:34:23 +0900

    [add] checkbox count

--- static/dist/printOrder.js
+++ static/dist/printOrder.js
@@ -8,43 +8,58 @@ checkboxChangeValue();
 let printHistories;
 async function main() {
     printHistories = await fetchPath(url + "/print");
-    const inputElem = document.getElementById("search-form");
-    const outputElem = document.getElementById("search-result");
-    inputElem?.addEventListener("keyup", () => {
-        while (outputElem?.firstChild) {
-            outputElem.removeChild(outputElem.firstChild);
-        }
-        const result = fzfSearchList(Object.keys(printHistories), inputElem.value);
-        result.forEach((key) => {
-            const option = document.createElement("option");
-            option.text = key;
-            option.value = key;
-            outputElem?.append(option);
-        });
-    });
-    outputElem?.addEventListener("change", (e) => {
-        const key = e.target.value;
-        const order = printHistories[key];
-        console.log(order);
-        document.getElementById("section").value = order["要求元"];
-        document.getElementById("order-no").value = order["生産命令番号"];
-        document.getElementById("order-name").value = order["生産命令名称"];
-        const drawNo = document.querySelectorAll("input[name='draw-no']");
-        drawNo.forEach((elem, i) => {
-            elem.value = order["図番"][i];
-        });
-        const drawName = document.querySelectorAll("input[name='draw-name']");
-        drawName.forEach((elem, i) => {
-            elem.value = order["図面名称"][i];
-        });
-        const drawQuant = document.querySelectorAll("input[name='quantity']");
-        drawQuant.forEach((elem, i) => {
-            elem.value = order["枚数"][i];
-        });
-        const drawMisc = document.querySelectorAll("input[name='misc']");
-        drawMisc.forEach((elem, i) => {
-            elem.value = order["備考"][i];
-        });
-        checkboxesToggle(order["必要箇所"]);
-    });
 }
+const inputElem = document.getElementById("search-form");
+const outputElem = document.getElementById("search-result");
+inputElem?.addEventListener("keyup", () => {
+    while (outputElem?.firstChild) {
+        outputElem.removeChild(outputElem.firstChild);
+    }
+    const result = fzfSearchList(Object.keys(printHistories), inputElem.value);
+    result.forEach((key) => {
+        const option = document.createElement("option");
+        option.text = key;
+        option.value = key;
+        outputElem?.append(option);
+    });
+});
+const drawNo = document.querySelectorAll("input[name='draw-no']");
+const drawName = document.querySelectorAll("input[name='draw-name']");
+const drawQuant = document.querySelectorAll("input[name='quantity']");
+const drawMisc = document.querySelectorAll("input[name='misc']");
+outputElem?.addEventListener("change", (e) => {
+    const key = e.target.value;
+    const order = printHistories[key];
+    console.log(order);
+    document.getElementById("section").value = order["要求元"];
+    document.getElementById("order-no").value = order["生産命令番号"];
+    document.getElementById("order-name").value = order["生産命令名称"];
+    drawNo.forEach((elem, i) => {
+        elem.value = order["図番"][i];
+    });
+    drawName.forEach((elem, i) => {
+        elem.value = order["図面名称"][i];
+    });
+    drawQuant.forEach((elem, i) => {
+        elem.value = order["枚数"][i];
+    });
+    drawMisc.forEach((elem, i) => {
+        elem.value = order["備考"][i];
+    });
+    checkboxesToggle(order["必要箇所"]);
+});
+const checkboxes = document.querySelectorAll("input[type='checkbox']");
+checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener("change", (e) => {
+        let checkedCount = 0;
+        checkboxes.forEach((checkbox) => {
+            if (checkbox.checked)
+                checkedCount++;
+        });
+        drawQuant.forEach((q, i) => {
+            if (drawNo[i].value !== "") {
+                q.value = checkedCount;
+            }
+        });
+    });
+});
--- static/src/printOrder.ts
+++ static/src/printOrder.ts
@@ -21,51 +21,67 @@ type PrintOrder = {
 
 async function main() {
   printHistories = await fetchPath(url + "/print");
-
-  const inputElem: HTMLElements = document.getElementById("search-form");
-  const outputElem = document.getElementById("search-result");
-  // formへの入力があるたびにoption書き換え
-  inputElem?.addEventListener("keyup", () => {
-    while (outputElem?.firstChild) { // clear option
-      outputElem.removeChild(outputElem.firstChild);
-    }
-    // Map.keys() メソッドがなぜか機能しないのでとりあえずObject.keys()使った。
-    const result: string[] = fzfSearchList(
-      Object.keys(printHistories),
-      inputElem.value,
-    );
-    result.forEach((key: string) => {
-      const option = document.createElement("option");
-      option.text = key;
-      option.value = key;
-      outputElem?.append(option);
-    });
-  });
-
-  // fzfの結果を出力したoptionを選択するたびに各フォームの書き換え
-  outputElem?.addEventListener("change", (e: Event) => {
-    const key: string = e.target.value;
-    const order: PrintOrder = printHistories[key];
-    console.log(order);
-    document.getElementById("section").value = order["要求元"];
-    document.getElementById("order-no").value = order["生産命令番号"];
-    document.getElementById("order-name").value = order["生産命令名称"];
-    const drawNo = document.querySelectorAll("input[name='draw-no']");
-    drawNo.forEach((elem: HTMLElement, i: number) => {
-      elem.value = order["図番"][i];
-    });
-    const drawName = document.querySelectorAll("input[name='draw-name']");
-    drawName.forEach((elem: string, i: number) => {
-      elem.value = order["図面名称"][i];
-    });
-    const drawQuant = document.querySelectorAll("input[name='quantity']");
-    drawQuant.forEach((elem: string, i: number) => {
-      elem.value = order["枚数"][i];
-    });
-    const drawMisc = document.querySelectorAll("input[name='misc']");
-    drawMisc.forEach((elem: string, i: number) => {
-      elem.value = order["備考"][i];
-    });
-    checkboxesToggle(order["必要箇所"]);
-  });
 }
+
+const inputElem: HTMLElements = document.getElementById("search-form");
+const outputElem = document.getElementById("search-result");
+// formへの入力があるたびにoption書き換え
+inputElem?.addEventListener("keyup", () => {
+  while (outputElem?.firstChild) { // clear option
+    outputElem.removeChild(outputElem.firstChild);
+  }
+  // Map.keys() メソッドがなぜか機能しないのでとりあえずObject.keys()使った。
+  const result: string[] = fzfSearchList(
+    Object.keys(printHistories),
+    inputElem.value,
+  );
+  result.forEach((key: string) => {
+    const option = document.createElement("option");
+    option.text = key;
+    option.value = key;
+    outputElem?.append(option);
+  });
+});
+
+// fzfの結果を出力したoptionを選択するたびに各フォームの書き換え
+const drawNo = document.querySelectorAll("input[name='draw-no']");
+const drawName = document.querySelectorAll("input[name='draw-name']");
+const drawQuant = document.querySelectorAll("input[name='quantity']");
+const drawMisc = document.querySelectorAll("input[name='misc']");
+outputElem?.addEventListener("change", (e: Event) => {
+  const key: string = e.target.value;
+  const order: PrintOrder = printHistories[key];
+  console.log(order);
+  document.getElementById("section").value = order["要求元"];
+  document.getElementById("order-no").value = order["生産命令番号"];
+  document.getElementById("order-name").value = order["生産命令名称"];
+  drawNo.forEach((elem: HTMLElement, i: number) => {
+    elem.value = order["図番"][i];
+  });
+  drawName.forEach((elem: string, i: number) => {
+    elem.value = order["図面名称"][i];
+  });
+  drawQuant.forEach((elem: string, i: number) => {
+    elem.value = order["枚数"][i];
+  });
+  drawMisc.forEach((elem: string, i: number) => {
+    elem.value = order["備考"][i];
+  });
+  checkboxesToggle(order["必要箇所"]);
+});
+
+// checkboxを変更するたびに、checkedされている数を枚数に反映
+const checkboxes = document.querySelectorAll("input[type='checkbox']");
+checkboxes.forEach((checkbox) => {
+  checkbox.addEventListener("change", (e: Event) => {
+    let checkedCount = 0;
+    checkboxes.forEach((checkbox) => {
+      if (checkbox.checked) checkedCount++;
+    });
+    drawQuant.forEach((q, i) => {
+      if (drawNo[i].value !== "") {
+        q.value = checkedCount;
+      }
+    });
+  });
+});
